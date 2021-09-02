0a50ae155d5bbf3603f560c2721400a5b12cb50f
LuizFilipeAS <luiz.filipedf@hotmail.com>
2021-09-02 02:32:44 -0300

    teste de mudanca

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ changes: 'were made' });
 }); //route handler
 
 const PORT = process.env.PORT || 5000;
